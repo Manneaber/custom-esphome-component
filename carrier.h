2f90b3ae3600ab37631ba70b4945e0d1875472cd
Manneaber <Manneaber@users.noreply.github.com>
2024-05-21 23:55:53 +0700

    save ac state to rom

--- carrier.h
+++ carrier.h
@@ -4,10 +4,10 @@
 #include "ir_Electra.h"
 #include <IRrecv.h>
 #include <IRutils.h>
+#include <EEPROM.h>
 
-
-const uint16_t kIrLed = 12;
-const uint16_t kRecvPin = 5;
+const uint16_t kIrLed = 4;
+const uint16_t kRecvPin = 14;
 const uint16_t kCaptureBufferSize = 1024;
 const uint8_t kTimeout = 50;
 
@@ -25,25 +25,102 @@ class CarrierAC : public PollingComponent, public Climate {
   }
 
   void setup() override {
+    EEPROM.begin(8);
+
     irrecv.enableIRIn(true);
 
+    bool acState = EEPROM.read(0); // 0x00
+    uint8_t acTemp = EEPROM.read(1); // 0x01
+    uint8_t acFan = EEPROM.read(2); // 0x02
+    uint8_t acMode = EEPROM.read(3); // 0x03
+    bool acSwingV = EEPROM.read(4); // 0x04
+    bool acSwingH = EEPROM.read(5); // 0x05
+    bool acLight = EEPROM.read(6); // 0x06
+
+    if (acTemp == 0) {
+      acTemp = 25;
+    }
+    if (acFan == 0) {
+      acFan = kElectraAcFanAuto;
+    }
+    if (acMode == 0) {
+      acMode = kElectraAcAuto;
+    }
+
     ac.begin();
-    ac.off();
-    ac.setPower(false);
-    ac.setTemp(25);
-    ac.setFan(kElectraAcFanAuto);
-    ac.setMode(kElectraAcAuto);
-    ac.setSwingV(true);
-    ac.setSwingH(true);
+
+    if (acState) {
+      ac.on();
+    } else {
+      ac.off();
+      ac.setPower(false);
+    }
+   
+    ac.setTemp(acTemp);
+    ac.setFan(acFan);
+    ac.setMode(acMode);
+    ac.setSwingV(acSwingV);
+    ac.setSwingH(acSwingH);
     ac.setLightToggle(false);
 
-    this->mode = CLIMATE_MODE_OFF;
-    this->target_temperature = 25;
-    this->fan_mode = CLIMATE_FAN_AUTO;
-    this->swing_mode = CLIMATE_SWING_BOTH;
-    lightSwitch->publish_state(true);
+    if (acState) {
+      switch (acMode) {
+        case kElectraAcAuto:
+        this->mode = CLIMATE_MODE_AUTO;
+        break;
+        case kElectraAcCool:
+        this->mode = CLIMATE_MODE_COOL;
+        break;
+        case kElectraAcDry:
+        this->mode = CLIMATE_MODE_DRY;
+        break;
+        case kElectraAcFan:
+        this->mode = CLIMATE_MODE_FAN_ONLY;
+        break;
+      }
+    } else {
+      this->mode = CLIMATE_MODE_OFF;
+    }
+
+    switch (acFan) {
+      case kElectraAcFanAuto:
+      this->fan_mode = CLIMATE_FAN_AUTO;
+      break;
+      case kElectraAcFanLow:
+      this->fan_mode = CLIMATE_FAN_LOW;
+      break;
+      case kElectraAcFanMed:
+      this->fan_mode = CLIMATE_FAN_MEDIUM;
+      break;
+      case kElectraAcFanHigh:
+      this->fan_mode = CLIMATE_FAN_HIGH;
+      break;
+    }
+
+    if (acSwingV && acSwingH) {
+      this->swing_mode = CLIMATE_SWING_BOTH;
+    } else if (acSwingV) {
+      this->swing_mode = CLIMATE_SWING_VERTICAL;
+    } else if (acSwingH) {
+      this->swing_mode = CLIMATE_SWING_HORIZONTAL;
+    } else {
+      this->swing_mode = CLIMATE_SWING_OFF;
+    }
+
+    this->target_temperature = acTemp;
+
+    lightSwitch->publish_state(acLight);
+    lastLightState = lightSwitch->state;
 
     this->publish_state();
+
+    ESP_LOGI("custom", "acState: %d", acState);
+    ESP_LOGI("custom", "acTemp: %d", acTemp);
+    ESP_LOGI("custom", "acFan: %d", acFan);
+    ESP_LOGI("custom", "acMode: %d", acMode);
+    ESP_LOGI("custom", "acSwingV: %d", acSwingV);
+    ESP_LOGI("custom", "acSwingH: %d", acSwingH);
+    ESP_LOGI("custom", "acLight: %d", acLight);
   }
 
   climate::ClimateTraits traits() {
@@ -85,47 +162,66 @@ class CarrierAC : public PollingComponent, public Climate {
       tempAC.setRaw(results.state);
 
       if (tempAC.getPower()) {
+        EEPROM.write(0, true);
+
         switch (tempAC.getMode()) {
           case kElectraAcAuto:
           this->mode = CLIMATE_MODE_AUTO;
+          EEPROM.write(3, kElectraAcAuto);
           break;
           case kElectraAcCool:
           this->mode = CLIMATE_MODE_COOL;
+          EEPROM.write(3, kElectraAcCool);
           break;
           case kElectraAcDry:
           this->mode = CLIMATE_MODE_DRY;
+          EEPROM.write(3, kElectraAcDry);
           break;
           case kElectraAcFan:
           this->mode = CLIMATE_MODE_FAN_ONLY;
+          EEPROM.write(3, kElectraAcFan);
           break;
         }
       } else {
         this->mode = CLIMATE_MODE_OFF;
+        EEPROM.write(0, false);
       }
 
       switch (tempAC.getFan()) {
         case kElectraAcFanAuto:
         this->fan_mode = CLIMATE_FAN_AUTO;
+        EEPROM.write(2, kElectraAcFanAuto);
         break;
         case kElectraAcFanLow:
         this->fan_mode = CLIMATE_FAN_LOW;
+        EEPROM.write(2, kElectraAcFanLow);
         break;
         case kElectraAcFanMed:
         this->fan_mode = CLIMATE_FAN_MEDIUM;
+        EEPROM.write(2, kElectraAcFanMed);
         break;
         case kElectraAcFanHigh:
         this->fan_mode = CLIMATE_FAN_HIGH;
+        EEPROM.write(2, kElectraAcFanHigh);
         break;
       }
 
       if (tempAC.getSwingV() && tempAC.getSwingH()) {
         this->swing_mode = CLIMATE_SWING_BOTH;
+        EEPROM.write(4, true);
+        EEPROM.write(5, true);
       } else if (tempAC.getSwingV()) {
         this->swing_mode = CLIMATE_SWING_VERTICAL;
+        EEPROM.write(4, true);
+        EEPROM.write(5, false);
       } else if (tempAC.getSwingH()) {
         this->swing_mode = CLIMATE_SWING_HORIZONTAL;
+        EEPROM.write(4, false);
+        EEPROM.write(5, true);
       } else {
         this->swing_mode = CLIMATE_SWING_OFF;
+        EEPROM.write(4, false);
+        EEPROM.write(5, false);
       }
 
       if (tempAC.getLightToggle()) {
@@ -142,6 +238,7 @@ class CarrierAC : public PollingComponent, public Climate {
     if (lightSwitch->state != lastLightState) {
         ac.setLightToggle(true);
         lastLightState = lightSwitch->state;
+        EEPROM.write(6, lastLightState);
 
         irrecv.disableIRIn();
         ac.send();
@@ -158,22 +255,31 @@ class CarrierAC : public PollingComponent, public Climate {
         case CLIMATE_MODE_COOL:
           ac.on();
           ac.setMode(kElectraAcCool);
+          EEPROM.write(0, true);
+          EEPROM.write(3, kElectraAcCool);
           break;
         case CLIMATE_MODE_AUTO:
           ac.on();
           ac.setMode(kElectraAcAuto);
+          EEPROM.write(0, true);
+          EEPROM.write(3, kElectraAcAuto);
           break;
         case CLIMATE_MODE_FAN_ONLY:
           ac.on();
           ac.setMode(kElectraAcFan);
+          EEPROM.write(0, true);
+          EEPROM.write(3, kElectraAcFan);
           break;
         case CLIMATE_MODE_DRY:
           ac.on();
           ac.setMode(kElectraAcDry);
+          EEPROM.write(0, true);
+          EEPROM.write(3, kElectraAcDry);
           break;
         case CLIMATE_MODE_OFF:
           ac.off();
           ac.setPower(false);
+          EEPROM.write(0, false);
           break;
       }
 
@@ -183,6 +289,7 @@ class CarrierAC : public PollingComponent, public Climate {
     if (call.get_target_temperature().has_value()) {
       float temp = *call.get_target_temperature();
       ac.setTemp(temp);
+      EEPROM.write(1, temp);
 
       this->target_temperature = temp;
     }
@@ -192,15 +299,19 @@ class CarrierAC : public PollingComponent, public Climate {
       switch(fanMode) {
         case CLIMATE_FAN_AUTO:
           ac.setFan(kElectraAcFanAuto);
+          EEPROM.write(2, kElectraAcFanAuto);
           break;
         case CLIMATE_FAN_LOW:
           ac.setFan(kElectraAcFanLow);
+          EEPROM.write(2, kElectraAcFanLow);
           break;
         case CLIMATE_FAN_MEDIUM:
           ac.setFan(kElectraAcFanMed);
+          EEPROM.write(2, kElectraAcFanMed);
           break;
         case CLIMATE_FAN_HIGH:
           ac.setFan(kElectraAcFanHigh);
+          EEPROM.write(2, kElectraAcFanHigh);
           break;
       }
 
@@ -213,18 +324,26 @@ class CarrierAC : public PollingComponent, public Climate {
         case CLIMATE_SWING_OFF:
           ac.setSwingV(false);
           ac.setSwingH(false);
+          EEPROM.write(4, false);
+          EEPROM.write(5, false);
           break;
         case CLIMATE_SWING_BOTH:
           ac.setSwingV(true);
           ac.setSwingH(true);
+          EEPROM.write(4, true);
+          EEPROM.write(5, true);
           break;
         case CLIMATE_SWING_VERTICAL:
           ac.setSwingV(true);
           ac.setSwingH(false);
+          EEPROM.write(4, true);
+          EEPROM.write(5, false);
           break;
         case CLIMATE_SWING_HORIZONTAL:
           ac.setSwingV(false);
           ac.setSwingH(true);
+          EEPROM.write(4, false);
+          EEPROM.write(5, true);
           break;
       }
 
@@ -232,6 +351,7 @@ class CarrierAC : public PollingComponent, public Climate {
     }
 
     this->publish_state();
+    EEPROM.commit();
 
     irrecv.disableIRIn();
     ac.send();

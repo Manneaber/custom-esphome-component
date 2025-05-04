bc2ecaf26c6a8e660c6a65c9050d30ec021b160a
Manneaber <Manneaber@users.noreply.github.com>
2025-05-04 11:27:51 +0000

    gbb

--- components/shift595_input/__init__.py
+++ components/shift595_input/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_DS_PIN, CONF_SRC_PIN, CONF_RCK_PIN
+from esphome.components import gpio
+from esphome.const import CONF_ID
 
 CODEOWNERS = []
 AUTO_LOAD = []
@@ -9,21 +9,25 @@ AUTO_LOAD = []
 shift595_ns = cg.namespace('shift595_input')
 Shift595Input = shift595_ns.class_('Shift595Input', cg.Component)
 
+# Pin configuration constants
 CONF_DS_PIN = "ds_pin"
 CONF_SRC_PIN = "src_pin"
 CONF_RCK_PIN = "rck_pin"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Shift595Input),
-    cv.Required(CONF_DS_PIN): cv.gpio_input_pin_schema,
-    cv.Required(CONF_SRC_PIN): cv.gpio_input_pin_schema,
-    cv.Required(CONF_RCK_PIN): cv.gpio_input_pin_schema,
+    cv.Required(CONF_DS_PIN): gpio.GPIO_PIN_SCHEMA,
+    cv.Required(CONF_SRC_PIN): gpio.GPIO_PIN_SCHEMA,
+    cv.Required(CONF_RCK_PIN): gpio.GPIO_PIN_SCHEMA,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    ds = await cg.gpio_pin_expression(config[CONF_DS_PIN])
-    src = await cg.gpio_pin_expression(config[CONF_SRC_PIN])
-    rck = await cg.gpio_pin_expression(config[CONF_RCK_PIN])
-    cg.add(var.set_pins(ds, src, rck))
+
+    # Get pin values from config and assign to the component
+    ds_pin = await cg.gpio_pin_expression(config[CONF_DS_PIN])
+    src_pin = await cg.gpio_pin_expression(config[CONF_SRC_PIN])
+    rck_pin = await cg.gpio_pin_expression(config[CONF_RCK_PIN])
+
+    cg.add(var.set_pins(ds_pin, src_pin, rck_pin))

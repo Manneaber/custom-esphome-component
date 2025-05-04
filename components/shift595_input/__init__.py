016a01f3ddf1fcbca0319b1c8806dcbd05f30d7f
Manneaber <Manneaber@users.noreply.github.com>
2025-05-04 11:31:02 +0000

    asdasd

--- components/shift595_input/__init__.py
+++ components/shift595_input/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import gpio
+from esphome import pins
 from esphome.const import CONF_ID
 
 CODEOWNERS = []
@@ -14,18 +14,19 @@ CONF_DS_PIN = "ds_pin"
 CONF_SRC_PIN = "src_pin"
 CONF_RCK_PIN = "rck_pin"
 
+# Define schema for our component's pins
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Shift595Input),
-    cv.Required(CONF_DS_PIN): gpio.GPIO_PIN_SCHEMA,
-    cv.Required(CONF_SRC_PIN): gpio.GPIO_PIN_SCHEMA,
-    cv.Required(CONF_RCK_PIN): gpio.GPIO_PIN_SCHEMA,
+    cv.Required(CONF_DS_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_SRC_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_RCK_PIN): pins.gpio_input_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Get pin values from config and assign to the component
+    # Get pin expressions from the configuration and assign to the component
     ds_pin = await cg.gpio_pin_expression(config[CONF_DS_PIN])
     src_pin = await cg.gpio_pin_expression(config[CONF_SRC_PIN])
     rck_pin = await cg.gpio_pin_expression(config[CONF_RCK_PIN])

538a2acc16dd8c303236009dbca22bec4229f708
Manneaber <Manneaber@users.noreply.github.com>
2025-05-04 11:23:18 +0000

    qq

---
+++ components/carrier42aaf2/shift595_input.cpp
@@ -0,0 +1,7 @@
+#include "shift595_input.h"
+
+namespace esphome {
+namespace shift595_input {
+
+}  // namespace shift595_input
+}  // namespace esphome
---
+++ components/carrier42aaf2/shift595_input.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace shift595_input {
+
+class Shift595Input : public Component {
+ public:
+  void set_pins(uint8_t ds_pin, uint8_t src_pin, uint8_t rck_pin) {
+    ds_pin_ = ds_pin;
+    src_pin_ = src_pin;
+    rck_pin_ = rck_pin;
+  }
+
+  void setup() override {
+    pinMode(ds_pin_, INPUT);
+    pinMode(src_pin_, INPUT);
+    pinMode(rck_pin_, INPUT);
+    last_src_ = digitalRead(src_pin_);
+    last_rck_ = digitalRead(rck_pin_);
+  }
+
+  void loop() override {
+    int current_src = digitalRead(src_pin_);
+    int current_rck = digitalRead(rck_pin_);
+
+    if (last_src_ == LOW && current_src == HIGH) {
+      shift_register_ = (shift_register_ << 1) | digitalRead(ds_pin_);
+    }
+
+    if (last_rck_ == LOW && current_rck == HIGH) {
+      output_register_ = shift_register_;
+      char bin_str[9];
+      for (int i = 7; i >= 0; i--) {
+        bin_str[7 - i] = (output_register_ & (1 << i)) ? '1' : '0';
+      }
+      bin_str[8] = '\0';
+      ESP_LOGD("shift595_input", "Latched value: %s (0x%02X)", bin_str, output_register_);
+    }
+
+    last_src_ = current_src;
+    last_rck_ = current_rck;
+  }
+
+ protected:
+  uint8_t ds_pin_, src_pin_, rck_pin_;
+  uint8_t shift_register_ = 0;
+  uint8_t output_register_ = 0;
+  int last_src_ = LOW;
+  int last_rck_ = LOW;
+};
+
+}  // namespace shift595_input
+}  // namespace esphome

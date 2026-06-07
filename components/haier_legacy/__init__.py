30c5244c09cb73d5d38d54196f0d1e976ac88a7a
Manneaber <Manneaber@users.noreply.github.com>
2026-06-07 23:48:47 +0700

    Delete components/haier_legacy directory

--- components/haier_legacy/__init__.py
+++
@@ -1,5 +0,0 @@
-# Haier (legacy) external component for ESPHome.
-#
-# This packages the original HaierFlexisPlus.h "custom" climate class as a
-# modern ESPHome external component, replacing the removed `platform: custom`
-# lambda mechanism.
